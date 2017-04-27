e112c5ecf0fddb508fc45a930161bebb43292602
Roman Pavliuk <sckalper@gmail.com>
2017-04-27 10:42:44 +0300

    #Skullper - have removed useless fvbid in custom views

--- app/src/main/java/com/a2lab/project/giftest/widgets/BottomFrameView.kt
+++ app/src/main/java/com/a2lab/project/giftest/widgets/BottomFrameView.kt
@@ -3,12 +3,12 @@ package com.a2lab.project.giftest.widgets
 import android.content.Context
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import com.a2lab.project.giftest.R
 import com.a2lab.project.giftest.widgets.adapters.BottomViewAdapter
+import kotlinx.android.synthetic.main.widget_bottom_view.view.*
 
 /**
  * Created by pugman on 07.03.17.
@@ -19,21 +19,18 @@ class BottomFrameView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
-    private val recyclerView: RecyclerView
     private lateinit var adapter: BottomViewAdapter
 
     init {
-        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val view = inflater.inflate(R.layout.widget_bottom_view, this, true)
-        recyclerView = view.findViewById(R.id.bottomRecyclerView) as RecyclerView
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        recyclerView.itemAnimator = DefaultItemAnimator()
+        LayoutInflater.from(this.context).inflate(R.layout.widget_bottom_view, this, true)
+        bottomRecyclerView.setHasFixedSize(true)
+        bottomRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        bottomRecyclerView.itemAnimator = DefaultItemAnimator()
     }
 
     fun setItems(items: ArrayList<Int>, frame: (Int) -> Unit) {
         adapter = BottomViewAdapter(items) { frame(it) }
-        recyclerView.adapter = adapter
+        bottomRecyclerView.adapter = adapter
     }
 
 }
--- app/src/main/java/com/a2lab/project/giftest/widgets/RoundedButton.kt
+++ app/src/main/java/com/a2lab/project/giftest/widgets/RoundedButton.kt
@@ -4,10 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
-import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
 import com.a2lab.project.giftest.R
+import kotlinx.android.synthetic.main.widget_round_button.view.*
 
 /**
  * Created by pugman on 09.03.17.
@@ -18,20 +17,15 @@ class RoundedButton @kotlin.jvm.JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
-    val title: TextView
-    val icon: ImageView
-
     init {
-        val root = LayoutInflater.from(this.context).inflate(R.layout.widget_round_button, this, true)
-        title = root.findViewById(R.id.widgetBut_text) as TextView
-        icon = root.findViewById(R.id.widgetBut_icon) as ImageView
+        LayoutInflater.from(this.context).inflate(R.layout.widget_round_button, this, true)
         orientation = LinearLayout.VERTICAL
         gravity = Gravity.CENTER
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.RoundedButton)
         val text = attributes.getText(R.styleable.RoundedButton_title)
         val drawable = attributes.getDrawable(R.styleable.RoundedButton_image)
-        title.text = text
-        icon.setImageDrawable(drawable)
+        this.widgetBut_text.text = text
+        this.widgetBut_icon.setImageDrawable(drawable)
         attributes.recycle()
     }
 }
